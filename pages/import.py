65212b9a41b957ce4b27ba064c2a98b21ee37104
kshitizkamra <68267771+kshitizkamra@users.noreply.github.com>
2024-08-08 19:48:18 +0530

    Update import.py

--- pages/import.py
+++ pages/import.py
@@ -14,41 +14,18 @@ conn = st.connection("my_database")
 
 
 
-col1, col2 = st.columns([2,1],gap="medium")
 
-with col1:
+
+container=st.container(border=True)
+
+with st.container(border=True) :
+   col1, col2 = st.columns([2,1],gap="medium")
+   with col1:
     uploaded_settlement = st.file_uploader(
     "Upload Settlement Files ", accept_multiple_files=True
     )
 
-    st.write("")
-    st.write("")
-
-    uploaded_sales = st.file_uploader(
-    "Upload Sales Files ", accept_multiple_files=True
-    )
-    st.write("")
-    st.write("")
-
-    uploaded_master = st.file_uploader(
-    "Upload Style Master Files ", accept_multiple_files=True
-    )
-    st.write("")
-    st.write("")
-
-    uploaded_action = st.file_uploader(
-    "Upload Style action Files ", accept_multiple_files=True
-    )
-
-    uploaded_actual_action = st.file_uploader(
-    "Upload actual action Files ", accept_multiple_files=True
-    )
-
-
-
-
-
-with col2:
+   with col2 :
     st.write("")
     st.write("")
     st.write("")
@@ -69,21 +46,27 @@ with col2:
        st.write(db_settlement.shape)
        db_settlement=db_settlement.drop_duplicates()
        st.write(db_settlement.shape)
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_90northbrands')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/myntra_roi')
        db_settlement.to_sql(
         name="final_settlement", # table name
         con=engine,  # engine
         if_exists="replace", #  If the table already exists, append
         index=False # no index
         )
-
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
+       
+ 
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
     
+    uploaded_sales = st.file_uploader(
+    "Upload Sales Files ", accept_multiple_files=True
+    )
+    
+  with col2 :
+    st.write("")
+    st.write("")
+    st.write("")
     if st.button('Upload',key="sales_btn"):
        db_sales=pd.DataFrame()
        db_sales_original=pd.DataFrame()
@@ -104,7 +87,7 @@ with col2:
        st.write(db_sales.shape)
        db_sales=db_sales.drop_duplicates()
        st.write(db_sales.shape)
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_90northbrands')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/myntra_roi')
        db_sales.to_sql(
         name="final_sales", # table name
         con=engine,  # engine
@@ -112,13 +95,16 @@ with col2:
         index=False # no index
         )    
     
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
+     uploaded_master = st.file_uploader(
+    "Upload Style Master Files ", accept_multiple_files=True
+    )
+  with col2 :
     st.write("")
     st.write("")
     st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    
     if st.button('Upload',key="master_btn"):
        db_master=pd.DataFrame()
        try :
@@ -138,12 +124,14 @@ with col2:
         if_exists="replace", #  If the table already exists, append
         index=False # no index
         )
-       
 
-       
-    st.write("")
-    st.write("")
-    st.write("")
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
+    uploaded_action = st.file_uploader(
+    "Upload Style action Files ", accept_multiple_files=True
+    )
+  with col2 :
     st.write("")
     st.write("")
     st.write("")
@@ -167,16 +155,17 @@ with col2:
         if_exists="replace", #  If the table already exists, append
         index=False # no index
         )
-       
 
-   
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
+    uploaded_actual_action = st.file_uploader(
+    "Upload actual action Files ", accept_multiple_files=True
+    )
+  with col2 :
     st.write("")
     st.write("")
     st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    
     if st.button('Upload',key="actual_action_btn"):
        db_actual_action=pd.DataFrame()
        try :
