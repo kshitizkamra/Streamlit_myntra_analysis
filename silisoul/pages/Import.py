45231950a8937a5855eab47fd6e921bb95a23277
kshitizkamra <68267771+kshitizkamra@users.noreply.github.com>
2024-08-08 21:36:52 +0530

    Update Import.py

--- silisoul/pages/Import.py
+++ silisoul/pages/Import.py
@@ -14,41 +14,18 @@ conn = st.connection("my_database")
 
 
 
-col1, col2 = st.columns([2,1],gap="medium")
 
-with col1:
+
+container=st.container(border=True)
+
+with st.container(border=True) :
+   col1, col2 = st.columns([2,1],gap="medium")
+   with col1:
     uploaded_settlement = st.file_uploader(
     "Upload Settlement Files ", accept_multiple_files=True
     )
 
-    st.write("")
-    st.write("")
-
-    uploaded_sales = st.file_uploader(
-    "Upload Sales Files ", accept_multiple_files=True
-    )
-    st.write("")
-    st.write("")
-
-    uploaded_master = st.file_uploader(
-    "Upload Style Master Files ", accept_multiple_files=True
-    )
-    st.write("")
-    st.write("")
-
-    uploaded_action = st.file_uploader(
-    "Upload Style action Files ", accept_multiple_files=True
-    )
-
-    uploaded_actual_action = st.file_uploader(
-    "Upload actual action Files ", accept_multiple_files=True
-    )
-
-
-
-
-
-with col2:
+   with col2 :
     st.write("")
     st.write("")
     st.write("")
@@ -69,21 +46,27 @@ with col2:
        st.write(db_settlement.shape)
        db_settlement=db_settlement.drop_duplicates()
        st.write(db_settlement.shape)
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_silisoul')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/myntra_roi')
        db_settlement.to_sql(
         name="final_settlement", # table name
         con=engine,  # engine
         if_exists="replace", #  If the table already exists, append
         index=False # no index
         )
-
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
+       
+ 
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
     
+    uploaded_sales = st.file_uploader(
+    "Upload Sales Files ", accept_multiple_files=True
+    )
+    
+  with col2 :
+    st.write("")
+    st.write("")
+    st.write("")
     if st.button('Upload',key="sales_btn"):
        db_sales=pd.DataFrame()
        db_sales_original=pd.DataFrame()
@@ -104,7 +87,7 @@ with col2:
        st.write(db_sales.shape)
        db_sales=db_sales.drop_duplicates()
        st.write(db_sales.shape)
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_silisoul')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/myntra_roi')
        db_sales.to_sql(
         name="final_sales", # table name
         con=engine,  # engine
@@ -112,13 +95,16 @@ with col2:
         index=False # no index
         )    
     
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
+     uploaded_master = st.file_uploader(
+    "Upload Style Master Files ", accept_multiple_files=True
+    )
+  with col2 :
     st.write("")
     st.write("")
     st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    
     if st.button('Upload',key="master_btn"):
        db_master=pd.DataFrame()
        try :
@@ -131,19 +117,21 @@ with col2:
        db_master.columns = [x.lower() for x in db_master.columns]
        db_master=pd.concat([db_master,db_master_original],ignore_index=True)
        db_master=db_master.drop_duplicates()
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_silisoul')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_90northbrands')
        db_master.to_sql(
         name="styles_master", # table name
         con=engine,  # engine
         if_exists="replace", #  If the table already exists, append
         index=False # no index
         )
-       
 
-       
-    st.write("")
-    st.write("")
-    st.write("")
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
+    uploaded_action = st.file_uploader(
+    "Upload Style action Files ", accept_multiple_files=True
+    )
+  with col2 :
     st.write("")
     st.write("")
     st.write("")
@@ -160,23 +148,24 @@ with col2:
        db_action.columns = [x.lower() for x in db_action.columns]
        db_action=pd.concat([db_action,db_action_original],ignore_index=True)
        db_action=db_action.drop_duplicates()
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_silisoul')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_90northbrands')
        db_action.to_sql(
         name="styles_action", # table name
         con=engine,  # engine
         if_exists="replace", #  If the table already exists, append
         index=False # no index
         )
-       
 
-   
+with st.container(border=True) : 
+  col1, col2 = st.columns([2,1],gap="medium")
+  with col1:
+    uploaded_actual_action = st.file_uploader(
+    "Upload actual action Files ", accept_multiple_files=True
+    )
+  with col2 :
     st.write("")
     st.write("")
     st.write("")
-    st.write("")
-    st.write("")
-    st.write("")
-    
     if st.button('Upload',key="actual_action_btn"):
        db_actual_action=pd.DataFrame()
        try :
@@ -189,7 +178,7 @@ with col2:
        db_actual_action.columns = [x.lower() for x in db_actual_action.columns]
        db_actual_action=pd.concat([db_actual_action,db_actual_action_original],ignore_index=True)
        db_actual_action=db_actual_action.drop_duplicates()
-       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_silisoul')
+       engine = create_engine('postgresql://90northbrands:90northbrands@34.41.36.17:5432/db_90northbrands')
        db_actual_action.to_sql(
         name="actual_actions", # table name
         con=engine,  # engine
